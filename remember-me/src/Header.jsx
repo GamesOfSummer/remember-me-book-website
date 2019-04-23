06e81ed204969642e49485fcc994f0663e50a058
Summer <summer.d.howard@gmail.com>
2019-04-23 14:46:37 -0500

    fixed image scaling

--- remember-me/src/Header.jsx
+++ remember-me/src/Header.jsx
@@ -1,33 +1,47 @@
 import React, { Component } from 'react';
 import image from './images/headerMain.jpg';
-import { MDBMask, MDBView, MDBContainer, MDBRow, MDBCol } from 'mdbreact';
+import {
+    MDBJumbotron,
+    MDBBtn,
+    MDBRow,
+    MDBCol,
+    MDBCardTitle,
+    MDBIcon
+} from 'mdbreact';
 
 export default class Header extends Component {
     render() {
         return (
             <div>
-                <MDBView waves>
-                    <img src={image} className="img-fluid" alt="" />
-                    <MDBMask className="flex-center">
-                        <div class="mask flex-center waves-effect waves-light rgba-grey-strong">
-                            <p class="white-text center">
+                <MDBJumbotron style={{ padding: 0 }}>
+                    <MDBCol
+                        className="text-white text-center py-5 px-4 my-5"
+                        style={{
+                            background:
+                                'url(' + image + ') fixed center no-repeat',
+                            'background-size': `cover`,
+                            opacity: '0.2'
+                        }}
+                    >
+                        <MDBCol className="py-5">
+                            <MDBCardTitle className="h1-responsive pt-3 m-5 font-bold">
+                                Remember Me
+                            </MDBCardTitle>
+                            <p className="mx-5 mb-5">
                                 {' '}
-                                <h1>
-                                    <b>Remember Me</b>
-                                </h1>
-                                <p>
-                                    <b>
-                                        Heart-warming, eye opening book promises
-                                        to change the way you see life, death,
-                                        and cancer through the touching true
-                                        stories, words, and sometimes death of
-                                        precious children in treatment
-                                    </b>
-                                </p>
+                                Heart-warming, eye opening book promises to
+                                change the way you see life, death, and cancer
+                                through the touching true stories, words, and
+                                sometimes death of precious children in
+                                treatment
                             </p>
-                        </div>
-                    </MDBMask>
-                </MDBView>
+                            <MDBBtn outline color="white" className="mb-5">
+                                <MDBIcon icon="clone" className="mr-2" /> View
+                                project
+                            </MDBBtn>
+                        </MDBCol>
+                    </MDBCol>
+                </MDBJumbotron>
             </div>
         );
     }
