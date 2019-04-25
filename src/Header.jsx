b6ee55948cbc1d2505b029a3d57114974931f336
Summer <summer.d.howard@gmail.com>
2019-04-25 09:37:56 -0500

    changing quotes out

--- src/Header.jsx
+++ src/Header.jsx
@@ -18,48 +18,37 @@ export default class Header extends Component {
             <div id="apppage">
                 <MDBView>
                     <MDBMask className="d-flex justify-content-center align-items-center gradient">
-                        <MDBContainer>
-                            <MDBAnimation type="fadeIn" delay="0.0s">
-                                <MDBRow>
-                                    <div className="white-text text-center text-md-left col-md-6 mt-xl-5 mb-5">
-                                        <h1 className="h1-responsive font-weight-bold mt-sm-5">
-                                            Remember Me{' '}
-                                        </h1>
-                                        <hr className="hr-light" />
-                                        <h6 className="mb-4">
-                                            Heart-warming, eye opening book
-                                            promises to change the way you see
-                                            life, death, and cancer through the
-                                            touching true stories, words, and
-                                            sometimes death of precious children
-                                            in treatment
-                                        </h6>
+                        <MDBAnimation type="fadeIn" delay="0.0s">
+                            <MDBRow>
+                                <div className="white-text text-center text-md-left col-md-6 mt-xl-5 mb-5">
+                                    <h1 className="h1-responsive font-weight-bold mt-sm-5">
+                                        Remember Me{' '}
+                                    </h1>
+                                    <hr className="hr-light" />
+                                    <h6 className="mb-4">
+                                        Heart-warming, eye opening book promises
+                                        to change the way you see life, death,
+                                        and cancer through the touching true
+                                        stories, words, and sometimes death of
+                                        precious children in treatment
+                                    </h6>
 
-                                        <MDBBtn gradient="winter-neva">
-                                            <a href="https://www.amazon.com/Remember-Me-Memoir-Children-Combating-ebook/dp/B07PFYN1KV/">
-                                                Buy on Amazon
-                                            </a>
-                                        </MDBBtn>
-                                        <MDBBtn gradient="winter-neva">
-                                            <a href="https://www.barnesandnoble.com/w/remember-me-janalee-tomaseski-heinemann-msw/1131142845?ean=2940161321973">
-                                                Buy on Barnes ahd Noble
-                                            </a>
-                                        </MDBBtn>
-                                    </div>
-                                    <MDBCol md="6" xl="5" className="mt-xl-5" />
-                                </MDBRow>
-                            </MDBAnimation>
-                        </MDBContainer>
+                                    <MDBBtn gradient="winter-neva">
+                                        <a href="https://www.amazon.com/Remember-Me-Memoir-Children-Combating-ebook/dp/B07PFYN1KV/">
+                                            Buy on Amazon
+                                        </a>
+                                    </MDBBtn>
+                                    <MDBBtn gradient="winter-neva">
+                                        <a href="https://www.barnesandnoble.com/w/remember-me-janalee-tomaseski-heinemann-msw/1131142845?ean=2940161321973">
+                                            Buy on Barnes ahd Noble
+                                        </a>
+                                    </MDBBtn>
+                                </div>
+                                <MDBCol md="6" xl="5" className="mt-xl-5" />
+                            </MDBRow>
+                        </MDBAnimation>
                     </MDBMask>
                 </MDBView>
-
-                <MDBContainer>
-                    <MDBRow className="py-5">
-                        <MDBCol md="12" className="text-center">
-                            <p />
-                        </MDBCol>
-                    </MDBRow>
-                </MDBContainer>
             </div>
         );
     }
--- src/Spacer.jsx
+++ src/Spacer.jsx
@@ -6,7 +6,12 @@ export default class Spacer extends Component {
         return (
             <div>
                 <MDBRow>
-                    <MDBCol md="6" className="mb-4">
+                    <MDBCol md="12" className="my-3">
+                        <p className="text-center">
+                            <h1>Testomonials</h1>
+                        </p>
+                    </MDBCol>
+                    <MDBCol md="6" className="mb-3">
                         <MDBAnimation reveal type="slideInLeft">
                             <MDBCard
                                 color="winter-neva-gradient"
@@ -14,19 +19,24 @@ export default class Spacer extends Component {
                                 className="text-center"
                             >
                                 <MDBCardBody>
-                                    Oh my gosh <br />
-                                    Your Mom <br />
-                                    So gifted <br />
-                                    So amazing <br />
-                                    What an immense blessing and legacy she is
-                                    giving to all those families with her book!
-                                    I got 1/2 way thru on the plane.... God
-                                    blessed her with such an awesome soul. -Ali
+                                    <p className="text-left">
+                                        {' '}
+                                        Oh my gosh <br />
+                                        Your Mom <br />
+                                        So gifted <br />
+                                        So amazing <br />
+                                        What an immense blessing and legacy she
+                                        is giving to all those families with her
+                                        book! I got 1/2 way thru on the
+                                        plane.... God blessed her with such an
+                                        awesome soul.
+                                    </p>
+                                    <p className="text-right">-Ali</p>
                                 </MDBCardBody>
                             </MDBCard>
                         </MDBAnimation>
                     </MDBCol>
-                    <MDBCol md="6" className="mb-4">
+                    <MDBCol md="6" className="mb-3">
                         <MDBAnimation reveal type="slideInRight">
                             <MDBCard
                                 color="grey lighten-1"
@@ -34,18 +44,24 @@ export default class Spacer extends Component {
                                 className="text-center"
                             >
                                 <MDBCardBody>
-                                    Absolutely love the book and I have been
-                                    meaning to email you. So beautifully written
-                                    and I will cherish the memories and “voice”
-                                    forever. I felt like I was right back
-                                    there... in a good way - and maybe for the
-                                    first time able to give myself that breath,
-                                    that we really did do it. It wasn’t some
-                                    black hole that closed up and was forgotten
-                                    about. All those friends, memories, and
-                                    stories - we all really existed.... and
-                                    someone remembered. Thank you � Emily
-                                    (cancer survivor in the book)
+                                    <p className="text-left">
+                                        {' '}
+                                        Absolutely love the book and I have been
+                                        meaning to email you. So beautifully
+                                        written and I will cherish the memories
+                                        and “voice” forever. I felt like I was
+                                        right back there... in a good way - and
+                                        maybe for the first time able to give
+                                        myself that breath, that we really did
+                                        do it. It wasn’t some black hole that
+                                        closed up and was forgotten about. All
+                                        those friends, memories, and stories -
+                                        we all really existed.... and someone
+                                        remembered. ​Thank you.
+                                    </p>
+                                    <p className="text-right">
+                                        - Emily (cancer survivor in the book)
+                                    </p>
                                 </MDBCardBody>
                             </MDBCard>
                         </MDBAnimation>
@@ -53,7 +69,7 @@ export default class Spacer extends Component {
                 </MDBRow>
 
                 <MDBRow>
-                    <MDBCol md="6" className="mb-4">
+                    <MDBCol md="6" className="mb-3">
                         <MDBAnimation reveal type="slideInLeft">
                             <MDBCard
                                 color="grey lighten-1"
@@ -61,20 +77,28 @@ export default class Spacer extends Component {
                                 className="text-center"
                             >
                                 <MDBCardBody>
-                                    Janalee! We received the book and THANK YOU!
-                                    ...for sending it and especially for a well
-                                    written tribute to these wonderful kids.
-                                    Your book is a work of love. We will also
-                                    never forget you and the kindness and caring
-                                    and REAL help for Jason and us too. Still
-                                    hard to believe all the amazing things you
-                                    did, and succeeded in getting others to do!
-                                    Larry (parent whose son is in the book)
+                                    <p className="text-left">
+                                        {' '}
+                                        The love shared through this author's
+                                        journey with these incredible children &
+                                        teens over 10 years have left a mark on
+                                        my soul. I won't forget their names
+                                        either. …Her last chapter hit hard
+                                        because it was about her son who I've
+                                        come to adore! His faith, courage and
+                                        transparency sharing his real and raw
+                                        journey is what will impact others. The
+                                        minute I started this book, I couldn't
+                                        put it down. May God continue to bless
+                                        Janalee, her family and all who struggle
+                                        with cancer! ​
+                                    </p>
+                                    <p className="text-right">Ginamarie M</p>
                                 </MDBCardBody>
                             </MDBCard>
                         </MDBAnimation>
                     </MDBCol>
-                    <MDBCol md="6" className="mb-4">
+                    <MDBCol md="6" className="mb-3">
                         <MDBAnimation reveal type="slideInRight">
                             <MDBCard
                                 color="winter-neva-gradient"
@@ -82,11 +106,22 @@ export default class Spacer extends Component {
                                 className="text-center"
                             >
                                 <MDBCardBody>
-                                    Wow. What a powerful book. I was both moved
-                                    by the stories and learned a lot from your
-                                    takeaways. Congrats again. You are truly
-                                    amazing in what you&#39;ve done for others
-                                    throughout your life. Rob Lutz
+                                    <p className="text-left">
+                                        {' '}
+                                        …a rare gift which allows us as we read
+                                        each page to live the passion and
+                                        poignancy of these daily interactions.
+                                        Moreover, this book provides invaluable
+                                        insight to health care professionals. We
+                                        face ongoing challenges in our work; but
+                                        we must “speak their name” and above all
+                                        remember they are not their disease, but
+                                        first and foremost, our patient and
+                                        “someone’s child”. ​
+                                    </p>
+                                    <p className="text-right">
+                                        Marilyn C. Dumont-Driscoll, PhD, MD
+                                    </p>
                                 </MDBCardBody>
                             </MDBCard>
                         </MDBAnimation>
