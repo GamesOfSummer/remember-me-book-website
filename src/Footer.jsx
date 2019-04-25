bcb70bfeb1269b6cca1cdde8f2ff14dff8b7f7aa
Summer <summer.d.howard@gmail.com>
2019-04-25 10:46:38 -0500

    new header image, footer colors

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { MDBContainer, MDBCol, MDBRow, MDBMedia, MDBAnimation } from 'mdbreact';
 
+import './Footer.css';
+
 export default class Footer extends Component {
     render() {
         return (
             <div>
-                <MDBRow>
+                <MDBRow id="footer">
                     <MDBCol size="12">
-                        <div class="d-flex justify-content-end">
+                        <div class="d-flex justify-content-end gradient">
                             <i class="my-2 mx-5 far fa-circle">
                                 {' '}
                                 <i>Adam's Angels LLC</i>
